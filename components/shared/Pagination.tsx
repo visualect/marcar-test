0cd44afd04d8699b95fa9989ef15762be735d60f
ian <yanikidev@gmail.com>
2025-06-15 17:03:47 +0200

    fix pagination

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -24,10 +24,12 @@ export default function Pagination({ meta }: IPagination) {
     router.push(pathname + `?${params.toString()}`)
   }
 
-  const totalPages = Math.ceil(meta.total / meta.count)
+  const TAKE = 12
+
+  const totalPages = Math.ceil(meta.total / TAKE)
 
   const pageNumbers = useMemo(() => {
-    const range = 2
+    const range = 1
     const pages: (number | string)[] = [1]
 
     if (totalPages <= 5)
@@ -57,26 +59,33 @@ export default function Pagination({ meta }: IPagination) {
     if (meta.page > 0) pushQueryString('page', meta.page - 1)
   }
 
+  const ARROW_BTN_STYLES =
+    'flex justify-center items-center rounded-[10px] aspect-square p-1 md:p-2 bg-blue-700 hover:bg-blue-500 text-white h-[36px] md:h-[40px] disabled:opacity-50 disabled:bg-gray-400 cursor-pointer ease-out transition-colors'
+
   return (
     <div className="flex justify-center gap-2 w-full py-4">
       <button
-        className="flex justify-center items-center rounded-[10px] aspect-square p-2 bg-blue-700 text-white h-[40px] cursor-pointer"
+        disabled={meta.page === 1}
+        className={ARROW_BTN_STYLES}
         onClick={handlePrevPage}
       >
-        <RiArrowLeftSLine />
+        <RiArrowLeftSLine size={16} />
       </button>
       <ul className="flex gap-2">
         {pageNumbers.map((num, idx) => {
           if (typeof num === 'string') {
             return (
-              <p className="self-end" key={num + idx}>
+              <p
+                className="self-end font-medium text-sm text-blue-700"
+                key={num + idx}
+              >
                 ...
               </p>
             )
           } else {
             return (
               <button
-                className={`flex justify-center items-center h-[40px] ${num === meta.page ? 'bg-blue-700 text-white hover:bg-blue-500' : 'bg-transparent text-blue-700 border border-blue-700 hover:border-blue-500 hover:bg-blue-500 hover:text-white'} rounded-[10px] aspect-square p-2 font-medium ease-out transition-colors text-sm  cursor-pointer`}
+                className={`flex justify-center items-center h-[36px] md:h-[40px] ${num === meta.page ? 'bg-blue-700 text-white hover:bg-blue-500' : 'bg-transparent text-blue-700 border border-blue-700 hover:border-blue-500 hover:bg-blue-500 hover:text-white'} rounded-[10px] aspect-square p-2 font-medium ease-out transition-colors text-xs md:text-sm cursor-pointer`}
                 onClick={() => pushQueryString('page', num)}
                 key={num}
               >
@@ -87,10 +96,11 @@ export default function Pagination({ meta }: IPagination) {
         })}
       </ul>
       <button
-        className="flex justify-center items-center rounded-[10px] aspect-square p-2 bg-blue-700 text-white h-[40px] cursor-pointer"
+        disabled={meta.page === totalPages}
+        className={ARROW_BTN_STYLES}
         onClick={handleNextPage}
       >
-        <RiArrowRightSLine />
+        <RiArrowRightSLine size={16} />
       </button>
     </div>
   )
